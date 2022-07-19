9ae358c3b7f5d5c5fb577c3e8be202e4c9e76df1
seo0yoon <genious921225@gmail.com>
2022-07-19 14:25:06 +0900

    Feat: CONTENT_URL 추가

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 ajax.open("GET", NEWS_URL, false); //동기적으로 처리
 ajax.send();
@@ -7,11 +10,32 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", function () {
+  const id = location.hash.substring(1);
+  console.log(id);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement("h1");
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
+  const a = document.createElement("a");
 
-  li.innerHTML = newsFeed[i].title;
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  a.addEventListener("click", function () {});
+
+  ul.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,21 +118,39 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+var container = document.getElementById("root");
 var ajax = new XMLHttpRequest();
+var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 ajax.open("GET", NEWS_URL, false); //동기적으로 처리
 
 ajax.send();
 var newsFeed = JSON.parse(ajax.response);
 var ul = document.createElement("ul");
+window.addEventListener("hashchange", function () {
+  var id = location.hash.substring(1);
+  console.log(id);
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  var title = document.createElement("h1");
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
 
 for (var i = 0; i < 10; i++) {
   var li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  var a = document.createElement("a");
+  a.href = "#".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  a.addEventListener("click", function () {});
+  ul.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
 },{}],"../../../../../Scoop/persist/nvm/nodejs/v16.14.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -161,7 +179,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51574" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51626" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
