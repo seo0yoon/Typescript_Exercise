3083b16b335435df8888fd53b8d00dfc2ff5f4ae
seo0yoon <genious921225@gmail.com>
2022-07-19 23:52:08 +0900

    Feat: add pagination

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -12,26 +15,34 @@ function getData(url) {
 }
 
 function newsFeed() {
+  //글 목록
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
 
   newsList.push("<ul>");
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-        <li>
-          <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-          </a>
-        </li> 
-      `);
+      <li>
+        <a href="#/show/${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li> 
+    `);
   }
   newsList.push("</ul>");
+  newsList.push(`
+  <div>
+   <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+   <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
+  </div>
+  `);
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1);
+  //글 내용
+  const id = location.hash.substring(7);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
@@ -39,7 +50,7 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href='#'>목록</a>
+      <a href='#/page/${store.currentPage}'>목록</a>
     </div>
 `;
 }
@@ -48,6 +59,9 @@ function router() {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
