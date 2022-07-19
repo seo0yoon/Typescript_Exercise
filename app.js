3120ea46f2f255c6dae57eeeacf7a4328b029872
seo0yoon <genious921225@gmail.com>
2022-07-19 23:14:58 +0900

    Feat: add  router

--- app.js
+++ app.js
@@ -4,38 +4,56 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false); //동기적으로 처리
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement("ul");
-
-window.addEventListener("hashchange", function () {
-  const id = location.hash.substring(1);
-  console.log(id);
-
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+function getData(url) {
+  ajax.open("GET", url, false);
   ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
-
-  content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-  a.addEventListener("click", function () {});
-
-  ul.appendChild(a);
-  ul.appendChild(li);
+  return JSON.parse(ajax.response);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+        <li>
+          <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          </a>
+        </li> 
+      `);
+  }
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
+  const id = location.hash.substring(1);
+
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href='#'>목록</a>
+    </div>
+`;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,34 +123,44 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-ajax.open("GET", NEWS_URL, false); //동기적으로 처리
 
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
-var ul = document.createElement("ul");
-window.addEventListener("hashchange", function () {
-  var id = location.hash.substring(1);
-  console.log(id);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+function getData(url) {
+  ajax.open("GET", url, false);
   ajax.send();
-  var newsContent = JSON.parse(ajax.response);
-  var title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-for (var i = 0; i < 10; i++) {
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  a.addEventListener("click", function () {});
-  ul.appendChild(a);
-  ul.appendChild(li);
+  return JSON.parse(ajax.response);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsFeed() {
+  var newsFeed = getData(NEWS_URL);
+  var newsList = [];
+  newsList.push("<ul>");
+
+  for (var i = 0; i < 10; i++) {
+    newsList.push("\n        <li>\n          <a href=\"#".concat(newsFeed[i].id, "\">\n            ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n          </a>\n        </li> \n      "));
+  }
+
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
+  var id = location.hash.substring(1);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href='#'>\uBAA9\uB85D</a>\n    </div>\n");
+}
+
+function router() {
+  var routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+router();
 },{}],"../../../../../Scoop/persist/nvm/nodejs/v16.14.0/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -179,7 +189,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51626" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51924" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
